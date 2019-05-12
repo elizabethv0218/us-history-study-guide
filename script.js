76c92cfdb6b7411b59fde6dfaa05daf014f71615
Elizabeth Vazquez <elizabethv0218@hstat.org>
2019-05-12 03:35:13 +0000

    fix link, erase comments, transfer jquery to script.js

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    $(".tab1").click(function() {
+        $(".tabInfo1").fadeToggle("slow");
+    });
+
+    $(".tab2").click(function() {
+        $(".tabInfo2").fadeToggle("slow");
+    });
+
+    $(".tab3").click(function() {
+        $(".tabInfo3").fadeToggle("slow");
+    });
+
+    $(".tab4").click(function() {
+        $(".tabInfo4").fadeToggle("slow");
+    });
+
+    $(".tab5").click(function() {
+        $(".tabInfo5").fadeToggle("slow");
+    });
+
+    $(".tab6").click(function() {
+        $(".tabInfo6").fadeToggle("slow");
+    });
+
+    $(".tab7").click(function() {
+        $(".tabInfo7").fadeToggle("slow");
+    });
+
+    $(".tab8").click(function() {
+        $(".tabInfo8").fadeToggle("slow");
+    });
+
+    $(".tab9").click(function() {
+        $(".tabInfo9").fadeToggle("slow");
+    });
+
+    $(".tab10").click(function() {
+        $(".tabInfo10").fadeToggle("slow");
+    });
+
+    $(".tab11").click(function() {
+        $(".tabInfo11").fadeToggle("slow");
+    });
+
+    $(".tab12").click(function() {
+        $(".tabInfo12").fadeToggle("slow");
+    });
+
+    $(".tab13").click(function() {
+        $(".tabInfo13").fadeToggle("slow");
+    });
+
+    $(".tab14").click(function() {
+        $(".tabInfo14").fadeToggle("slow");
+    });
+
+    $(".tab15").click(function() {
+        $(".tabInfo15").fadeToggle("slow");
+    });
+
+    $(".tab16").click(function() {
+        $(".tabInfo16").fadeToggle("slow");
+    });
+
+    $(".tab17").click(function() {
+        $(".tabInfo17").fadeToggle("slow");
+    });
+});
